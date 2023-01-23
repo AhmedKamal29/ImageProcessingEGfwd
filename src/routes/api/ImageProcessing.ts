a276f8a84189504a31e6584cb8f482ec33f8e29d
AhmedKamal29 <ahmed.kamal29@outlook.com>
2023-01-23 13:20:29 +0200

    adding explicit typing to api params & modifications in package.json

--- src/routes/api/ImageProcessing.ts
+++ src/routes/api/ImageProcessing.ts
@@ -6,43 +6,47 @@ import MainDir from '../../index';
 
 const ImageProcessing = express.Router();
 
-ImageProcessing.get('/resize', validate, async (req, res): Promise<void> => {
-  try {
-    let ImageName: string = String(req.query.name);
-    let ImageWidth: number = Number(req.query.width);
-    let ImageHeight: number = Number(req.query.height);
+ImageProcessing.get(
+  '/resize',
+  validate,
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    try {
+      let ImageName: string = String(req.query.name);
+      let ImageWidth: number = Number(req.query.width);
+      let ImageHeight: number = Number(req.query.height);
 
-    if (!search(ImageName, ImageWidth, ImageHeight)) {
-      let InputImage = `./src/assets/Images/${ImageName}.jpg`;
-      let OutputImage = `./src/assets/Thumb/${ImageName}_${ImageWidth}_${ImageHeight}.jpg`;
+      if (!search(ImageName, ImageWidth, ImageHeight)) {
+        let InputImage = `./src/assets/Images/${ImageName}.jpg`;
+        let OutputImage = `./src/assets/Thumb/${ImageName}_${ImageWidth}_${ImageHeight}.jpg`;
 
-      const ResizedImage = await resize(
-        InputImage,
-        ImageWidth,
-        ImageHeight,
-        OutputImage
-      );
+        const ResizedImage = await resize(
+          InputImage,
+          ImageWidth,
+          ImageHeight,
+          OutputImage
+        );
 
-      if (!ResizedImage) {
-        res.send('Opps seems like this image is not there 🫢');
-        return;
+        if (!ResizedImage) {
+          res.send('Opps seems like this image is not there 🫢');
+          return;
+        }
+
+        res.sendFile(
+          MainDir.__dirname +
+            `/assets/Thumb/${ImageName}_${ImageWidth}_${ImageHeight}.jpg`
+        );
+      } else {
+        res.sendFile(
+          MainDir.__dirname +
+            `/assets/Thumb/${ImageName}_${ImageWidth}_${ImageHeight}.jpg`
+        );
       }
-
-      res.sendFile(
-        MainDir.__dirname +
-          `/assets/Thumb/${ImageName}_${ImageWidth}_${ImageHeight}.jpg`
-      );
-    } else {
-      res.sendFile(
-        MainDir.__dirname +
-          `/assets/Thumb/${ImageName}_${ImageWidth}_${ImageHeight}.jpg`
+    } catch (e) {
+      res.send(
+        '<h2 style="text-align:center; margin:5%"> You need to enter query paramters for the image name and the requied size 🤷🏻‍♂️ !!</h2>'
       );
     }
-  } catch (e) {
-    res.send(
-      '<h2 style="text-align:center; margin:5%"> You need to enter query paramters for the image name and the requied size 🤷🏻‍♂️ !!</h2>'
-    );
   }
-});
+);
 
 export default ImageProcessing;
